a9c47d4b02c443ec1a9eda6d75a04788ccffc9a9
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-03-08 12:34:04 +0100

    adding js

--- dataBase.js
+++ dataBase.js
@@ -3,6 +3,14 @@
 
 let dataBase = {
   Avantasia: {
-    album: 'Moonglow',
+    Moonglow: {
+      album: 'Moonglow',
+      artist: 'Avantasia',
+      year: 2019,
+      genre: 'Heavy Metal',
+      subgenre: 'Symphonic Power Metal',
+      spotifyPlaylist: '<iframe src="https://open.spotify.com/embed/album/5p7e20MF6t7Z915ESKsjPq" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
+      totalSongs: 12,
+    }
   }
 };
--- script.js
+++ script.js
@@ -9,5 +9,14 @@ $("#Avantasia-Moonglow").on('hidden.bs.modal', ()=> {
 
 
 $("#modalButton").click( ()=> {
-    let name = $("");
+    let name = $("#Avantasia-Moonglow").attr("id");
+    
+    if(name === 'Avantasia-Moonglow') {
+        $("#details-modal__album").html(dataBase.Avantasia.Moonglow.album);
+        $("#details-modal__artist").html(dataBase.Avantasia.Moonglow.artist);
+        $("#details-modal__year").html(dataBase.Avantasia.Moonglow.year);
+        $("#details-modal__genre").html(dataBase.Avantasia.Moonglow.genre);
+        $("#details-modal__subgenre").html(dataBase.Avantasia.Moonglow.subgenre);
+        $("#details-modal__spotifyPlaylist").html(dataBase.Avantasia.Moonglow.spotifyPlaylist);
+    };
   });
