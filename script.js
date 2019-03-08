7653fbb428f889417610e195e6d7ed94c77a0b1f
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-03-08 10:40:30 +0100

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+// This instructions are for making the music from inside the details-modal to stop when the modal is closed
+
+//Instruction to stop the music from the Avantasia - Moonglow album
+$("#Avantasia-Moonglow").on('hidden.bs.modal', ()=> {
+    $("#Avantasia-Moonglow iframe").attr("src", $("#Avantasia-Moonglow iframe").attr("src"));
+});
